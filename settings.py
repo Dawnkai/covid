9cdd30db1395ffaf47654a1df35032f884f51f38
Cubaster <68557891+Cubaster@users.noreply.github.com>
2020-08-03 17:38:23 +0200

    Update settings.py
    H i L muszą być sobie równe

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ DISPLAY_SIZE = os.getenv("DISPLAY_SIZE", [650, 790])
 
 # Simulation settings
 BACKGROUND_IMAGE = os.getenv("BACKGROUND_IMAGE", "static/images/background.jpg")
-CONTAINER_SIZE = os.getenv("CONTAINTER_SIZE", (600, 400))
+CONTAINER_SIZE = os.getenv("CONTAINTER_SIZE", (600, 600))
 TICK_FRAMES = os.getenv("TICK_FRAMES", 60)
